2c73af6bc5797840e5cdf9055c316c9d5603a436
lpewton <liampewton99@gmail.com>
2023-10-10 13:42:41 +0000

    Add rent into consideration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,15 @@ import React, { useState } from 'react';
 function App() {
 
   const [inputValue, setInputValue] = useState('');
-  const perDiam = Math.floor((inputValue - 350) / 7)
+  const [rentValue, setrentValue] = useState('');
+  const perDiam = Math.floor((inputValue - 125 - rentValue / 4) / 7)
 
   const InputChange = (event) => {
     setInputValue(event.target.value);
+  };
+
+  const rentChange = (event) => {
+    setrentValue(event.target.value);
   };
 
   return (
@@ -16,15 +21,27 @@ function App() {
     <div>
       <h1>Money Calculator</h1>
     </div>
+    <div>
+      <label>Wage/week</label>
     <input
-    type="text"
-    placeholder="Money Received"
+    type="integer"
+    placeholder="Money Received/week"
     value={inputValue}
     onChange={InputChange}
     className='newChoreInput'
   />
-  <button>SUBMIT</button>
-  <p>You can spend £{perDiam} per day, saving £125 a week, counting £225 rent a month</p>
+  </div>
+  <div>
+  <label>Rent/month</label>
+  <input
+    type="integer"
+    placeholder="Rent/month"
+    value={rentValue}
+    onChange={rentChange}
+    className='newChoreInput'
+  />
+  </div>
+  <p>You can spend £{perDiam} per day, saving £125 a week</p>
   </div>
   </div>
   );
